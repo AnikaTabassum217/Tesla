7e53b047afc26713db4458ca63065046ce4a9d96
anika <anika@newgen-bd.com>
2023-04-11 10:13:34 +0600

    Second commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Head from './components/Head';
-import Navbar from './components/Navbar';
 import Page from './components/Page';
 
 function App() {
   return (
     <div>
-      <Head/>
-      
+      <Head/>     
       <Page/>
-      <Navbar/>
+     
     </div>
   );
 }
